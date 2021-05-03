350b1c9f41355308aa216c747fa3d3024e50bd23
Martin Neitz <martin.neitz@b-coop.at>
2021-05-03 14:57:02 +0200

    consolidation

---
+++ index.mjs
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2021.
+ */
+
+/*
+ *
+ * @author: Martin Neitz
+ */
+
+export { default as ServiceAgent } from './lib/serviceagent.mjs';
+export { default as ServiceAgentDescriptor } from './lib/serviceagentdescriptor.mjs';
