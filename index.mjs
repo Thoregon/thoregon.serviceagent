ef70c73bb6508307addff9ae914975ed0def8259
Bernhard Lukassen <bl@bernhard-lukassen.com>
2021-05-12 13:41:49 +0200

    introduce service descriptors

--- index.mjs
+++ index.mjs
@@ -7,5 +7,6 @@
  * @author: Martin Neitz
  */
 
-export { default as ServiceAgent } from './lib/serviceagent.mjs';
-export { default as ServiceAgentDescriptor } from './lib/serviceagentdescriptor.mjs';
+export { default as ServiceAgent }                  from './lib/serviceagent.mjs';
+export { default as ServiceAgentDescriptor }        from './lib/sadescriptor.mjs';
+export { default as ServiceAgentWebsiteDescriptor } from './lib/sawebsitedescriptor.mjs';
