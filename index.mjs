66ef5f782deebb9ef2e650dc025db8416747b7bf
Bernhard Lukassen <bl@bernhard-lukassen.com>
2022-02-22 11:14:00 +0100

    agent services, messengers

--- index.mjs
+++ index.mjs
@@ -6,4 +6,4 @@
 
 export { default as ServiceAgent }                  from './lib/serviceagent.mjs';
 export { default as ServiceAgentDescriptor }        from './lib/sadescriptor.mjs';
-export { default as ServiceAgentWebsiteDescriptor } from './lib/sawebsitedescriptor.mjs';
+// export { default as ServiceAgentWebsiteDescriptor } from './lib/sawebsitedescriptor.mjs';
